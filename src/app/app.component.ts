f446ac877be37dd9ce6f4fdbcef8781432fb7843
Dwipayan Tripathy <Dwipayan Tripathy>
2017-08-20 16:54:21 +0530

    Login ADD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
+import { LoginComponent } from './login/login.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  myvar="Hiii";
+  myvar = "Hiii";
   title = 'app works!';
 }
